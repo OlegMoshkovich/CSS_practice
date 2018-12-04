5120b6bc7b8e831d24db89751a0cb8fc12e5d853
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-04 11:13:59 -0500

    added closure example

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ array.forEach(function (el,index) {
     console.log(el);
   }, index * 2000);
 })
+
+
+//Closures
+
+// function setColor(color){
+//   var elColor = color;
+//   console.log(elColor)
+//   // console.log(elColor)
+//   return function(){
+//     if (elColor === 'green'){
+//        document.querySelector('.box').style.color = 'green';
+//     }else{
+//        console.log('inner function'+elColor)
+//        return document.querySelector('.box').style.color = 'red';
+//     }
+//   }
+// }
+//
+// function execute(){
+//   var color = document.querySelector('.textbox_id').value;
+//   setColor(color)();
+// }
+//
+// document.querySelector('.button').addEventListener("click", execute);
