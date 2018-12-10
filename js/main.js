b89b2cb687bf3d93d5d72dfca50a8cf9b9214033
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-10 15:52:42 -0500

    added navigation points

--- js/main.js
+++ js/main.js
@@ -2,14 +2,38 @@
 
 var array = ['Frontend engineering.', 'Product Design.',  'Balance.'];
 
+function disappear(){
+  console.log('run')
+  document.querySelector('.navigation__button-link2').style.display = 'none'
 
-
-array.forEach(function (el,index) {
-  setTimeout(function () {
-    document.querySelector('#focus').innerHTML = el;
-    console.log(el);
-  }, index * 2000);
+}
+document.querySelector('.navigation__button-link3').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link3').style.display = 'none'
 })
+document.querySelector('.navigation__button-link1').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link1').style.display = 'none'
+})
+document.querySelector('.navigation__button-link').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link').style.display = 'none'
+})
+document.querySelector('.navigation__button-link4').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link4').style.display = 'none'
+})
+document.querySelector('.navigation__button-link4').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link4').style.display = 'none'
+})
+document.querySelector('.navigation__button-link5').addEventListener("click", function(){
+  document.querySelector('.navigation__button-link5').style.display = 'none'
+})
+
+document.querySelector('.bg-video__content').defaultPlaybackRate = .5;
+
+// array.forEach(function (el,index) {
+//   setTimeout(function () {
+//     document.querySelector('#focus').innerHTML = el;
+//     console.log(el);
+//   }, index * 2000);
+// })
 
 document.querySelector('.bg-video__content').defaultPlaybackRate = .5;
 // document.querySelector('video').play()
