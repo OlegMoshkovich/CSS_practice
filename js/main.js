fefb069d0015ad9e46a550309243ee2832889b45
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-11 16:07:11 -0500

    major update to everything

--- js/main.js
+++ js/main.js
@@ -2,29 +2,26 @@
 
 var array = ['Frontend engineering.', 'Product Design.',  'Balance.'];
 
-function disappear(){
-  console.log('run')
-  document.querySelector('.navigation__button-link2').style.display = 'none'
 
-}
-document.querySelector('.navigation__button-link3').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link3').style.display = 'none'
-})
-document.querySelector('.navigation__button-link1').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link1').style.display = 'none'
-})
-document.querySelector('.navigation__button-link').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link').style.display = 'none'
-})
-document.querySelector('.navigation__button-link4').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link4').style.display = 'none'
-})
-document.querySelector('.navigation__button-link4').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link4').style.display = 'none'
-})
-document.querySelector('.navigation__button-link5').addEventListener("click", function(){
-  document.querySelector('.navigation__button-link5').style.display = 'none'
-})
+// document.querySelector('.navigation__button-link3').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link3').style.display = 'none'
+// })
+// document.querySelector('.navigation__button-link2').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link2').style.display = 'none'
+// })
+// document.querySelector('.navigation__button-link1').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link1').style.display = 'none'
+// })
+// document.querySelector('.navigation__button-link').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link').style.display = 'none'
+// })
+// document.querySelector('.navigation__button-link4').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link4').style.display = 'none'
+// })
+//
+// document.querySelector('.navigation__button-link5').addEventListener("click", function(){
+//   document.querySelector('.navigation__button-link5').style.display = 'none'
+// })
 
 document.querySelector('.bg-video__content').defaultPlaybackRate = .5;
 
